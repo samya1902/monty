720c2a5ad0f23b96ae84b416ca15460ce5dca75d
Hassnae BADDOU <samya1902@gmail.com>
2023-08-24 02:53:41 -0700

    pint

---
+++ pint.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+/**
+ * f_pint - prints the top
+ * @head: head
+ * @counter: nbr
+ * Return: void
+ */
+void f_pint(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stackempty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
+/**
+ * free_stack - frees doubly linked list
+ * @head: head of stack
+ * Return: void
+ */
+void free_stack(stack_t *head)
+{
+	stack_t *aux;
+
+	aux = head;
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+}
